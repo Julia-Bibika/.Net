56a3b44e0c2b6ee982d4d6c9dc48c8ec5e30e462
Julia-Bibika <93942521+Julia-Bibika@users.noreply.github.com>
2023-03-29 09:51:03 +0300

    module

---
+++ module/WebApplicationModule/WebApplicationModule/Controllers/Vector1Controller.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplicationModule.Controllers
+{
+    public class Vector1Controller : ControllerBase
+    {
+        [HttpGet("ex1")]
+        public async Task<ActionResult<double>> GetScalar(double[] x, double[] y)
+        {
+            double[] z = new double[x.Length];
+
+            if (x.Length != y.Length)
+            {
+                return BadRequest("Вектори повинні мати однаковий розмір.");
+            }
+
+            for (int i = 0; i < x.Length; i++)
+            {
+                z[i] = x[i] * y[i];
+            }
+
+            return Ok(z);
+        }
+        [HttpGet("ex2")]
+        public async Task<ActionResult<double>> GetSin(int n, double x)
+        {
+            if (n < 1)
+            {
+                return BadRequest(400);
+            }
+            double result = 0;
+            for (int i = 1; i <= n; i++)
+            {
+                result += Math.Pow(Math.Sin(x), i);
+            }
+            return Ok(result);
+        }
+    }
+}
