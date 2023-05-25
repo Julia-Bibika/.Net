db8298d3856ceb90bd1a9d05cd6efdfa33593511
Julia-Bibika <93942521+Julia-Bibika@users.noreply.github.com>
2023-05-25 22:01:52 +0300

    Lab4

--- Lab4/Lab2/Lab2/Services/Repositories/PostsRepositoryService.cs
+++ Lab4/Lab2/Lab2/Services/Repositories/PostsRepositoryService.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using Microsoft.EntityFrameworkCore;
 using Lab2.Data;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Lab2.Services.Repositories
 {
@@ -26,10 +27,13 @@ namespace Lab2.Services.Repositories
 
         public void Delete(int id)
         {
+            // to-do: додати можливість видалення поста за ідентифікатором
         }
 
         public List<Post> Read(Post filterBy, string orderBy, string order, int page, int perPage)
         {
+            // to-do: додати можливість фільтрувати пости за заголовком чи вмістом
+            // to-do: додати можливість сортувати пости за заголовком або датою публікації
 
             return context.Posts.OrderBy(post => post.Id).Skip((page - 1) * perPage).Take(perPage).ToList();
         }
@@ -39,12 +43,25 @@ namespace Lab2.Services.Repositories
             return context.Posts.Include(post => post.Comments).FirstOrDefault(post => post.Id == id);
         }
 
-        public void Update(Post post)
+        public void Update(Post updatedPost)
         {
+            Post post = context.Posts.FirstOrDefault(p => p.Id == updatedPost.Id);
+
+            if (post != null)
+            {
+                post.Title = updatedPost.Title;
+                post.Content = updatedPost.Content;
+
+            }
+            else
+            {
+                Console.WriteLine("Пост з вказаним ідентифікатором не знайдений.");
+            }
         }
 
         public int Count(Post filterBy)
         {
+            // to-do: додати значення фільтра при підрахунку
 
             return context.Posts.Count();
         }
@@ -57,14 +74,27 @@ namespace Lab2.Services.Repositories
             return entityEntry.Entity;
         }
 
-        public void UpdateCommentToPost(int id)
+        public void UpdateCommentToPost(int id, [FromBody] string newContent)
         {
+            // to-do: додати можливість редагувати коментар за ідентифікатором
+
+            CommentToPost commentToUpdate = context.Comments.FirstOrDefault(c => c.Id == id);
+            if (commentToUpdate != null)
+            {
+                commentToUpdate.Content = newContent;
+                commentToUpdate.PublishedOn = DateTime.Now;
+            }
+            else
+            {
+                Console.WriteLine("Коментар з вказаним ідентифікатором не знайдений.");
+            }
         }
 
-        public void DeleteCommentToPost(int id)
+        public void DeleteCommentToPost(int id, [FromBody] User user)
         {
+            // to-do: додати можливість видаляти коментар за ідентифікатором
         }
-        public bool IsAuthorizedToDelete(User user, CommentToPost comment)
+        public bool IsAuthorizedToDelete([FromBody] User user, CommentToPost comment)
         {
             // Логіка перевірки прав доступу користувача
             if (user.isAdmin == true)
